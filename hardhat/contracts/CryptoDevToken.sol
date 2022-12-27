e3b11a7836c42cb0aaef78a1dfa7cb6c4ba5af1b
Guilherme Costa <guilhermecosta.ds1@gmail.com>
2022-12-27 20:09:52 -0300

    Config build and  deploy of contract

--- hardhat/contracts/CryptoDevToken.sol
+++ hardhat/contracts/CryptoDevToken.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  ^0.8.0
+pragma solidity  ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ICryptoDevs.sol"
+import "./ICryptoDevs.sol";
 
 contract CryptoDevToken is ERC20, Ownable {
   uint256 public constant tokenPrice = 0.001 ether;
   uint256 public constant tokensPerNFT = 10 * 10 ** 18;
+  uint256 public constant maxTotalSupply = 10000 * 10**18;
   ICryptoDevs CryptoDevsNFT;
   mapping(uint256 => bool) public tokenIdsClaimed;
 
@@ -32,7 +33,8 @@ contract CryptoDevToken is ERC20, Ownable {
     uint256 amount = 0;
     
     for (uint256 i = 0; i< balance; i++){
-      uint256 tokenId CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+      uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+
       if(!tokenIdsClaimed[tokenId]){
         amount +=1;
         tokenIdsClaimed[tokenId] = true;
@@ -53,6 +55,6 @@ contract CryptoDevToken is ERC20, Ownable {
     require(sent, "Failed to send Ether");
   }
 
-  receive() external payable();
-  fallback() external payable();
+  receive() external payable{}
+  fallback() external payable{}
 }
